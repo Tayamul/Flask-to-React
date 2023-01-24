7e9d0bf9271eac414e3071360e9983ae8d1f5914
Tayamul <rai_tayamul@hotmail.com>
2023-01-24 14:49:11 +0000

    Link Flask to React

--- api/api.py
+++ api/api.py
@@ -1,7 +1,8 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Home page"
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
